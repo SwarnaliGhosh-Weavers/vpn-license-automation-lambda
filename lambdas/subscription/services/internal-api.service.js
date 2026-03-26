3327f906ad3ded5701d2c6cc586ee0ebf06c3d8f
SwarnaliGhosh-Weavers <swarnali.ghosh@weavers-web.com>
2026-03-26 17:25:05 +0530

    fix: work on shot suggetion messages

--- lambdas/subscription/services/internal-api.service.js
+++ lambdas/subscription/services/internal-api.service.js
@@ -2,34 +2,33 @@ const axios = require('axios');
 const { getConfig } = require('./config.service');
 const logger = require('../utils/logger');
 
-async function callInternalAPI(data) {
+async function callInternalAPI(customer, subscription) {
   try {
     const config = await getConfig();
 
     logger.info('Calling internal API', {
       url: config.internalApiUrl,
-      data,
+      customer, subscription,
     });
 
     const response = await axios.post(
       config.internalApiUrl,
-      data,
+      { customer, subscription },
       {
         headers: { 'Content-Type': 'application/json' },
         timeout: 5000,
       },
     );
 
-    logger.info('Internal API success', {
-      status: response.status,
-      data: response.data,
-    });
+    // logger.info('Internal API success', {
+    //   status: response.status,
+    //   data: response.data,
+    // });
 
     // return response.data;
 
   } catch (error) {
     logger.error('Internal API failed', {
-      url: config?.internalApiUrl,
       error: error.message,
       status: error.response?.status,
       data: error.response?.data,
